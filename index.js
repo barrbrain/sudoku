6efcaf72c6c4a119823abc4832a8442dd2811ef4
David Michael Barr <b@rr-dav.id.au>
2023-09-16 23:57:54 +0900

    Implement incremental rendering to canvas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { memory } from './pkg/index_bg.wasm';
 import { assign, generate_instance, solve, units_ptr, units_len } from './pkg/index.js';
 const UNITS = units_len();
 const units = () => new Uint32Array(memory.buffer, units_ptr(), UNITS);
+const renderedUnits = new Uint32Array(UNITS);
 
 const CELL_SIZE = 16;
 const GRID_COLOR = "#CCCCCC";
@@ -15,14 +16,15 @@ canvas.width = (CELL_SIZE + 1) * SIDE + 1;
 
 const ctx = canvas.getContext('2d');
 
+let renderLoopRequestID = 0;
 const renderLoop = () => {
-  drawGrid();
   drawCells();
-
-  requestAnimationFrame(renderLoop);
+  renderLoopRequestID = requestAnimationFrame(renderLoop);
 };
 
 const drawGrid = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   ctx.beginPath();
   ctx.strokeStyle = GRID_COLOR;
 
@@ -37,6 +39,9 @@ const drawGrid = () => {
   }
 
   ctx.stroke();
+
+  renderedUnits.fill(0);
+  renderLoop();
 };
 
 const getIndex = (row, column) => {
@@ -45,46 +50,53 @@ const getIndex = (row, column) => {
   const VAL = (row - ROW * 3) * 3 + column - COL * 3;
   return (ROW * 9 + COL) * 9 + VAL;
 };
-
+const toRow = new Uint8Array(SIDE * SIDE);
+const toCol = new Uint8Array(SIDE * SIDE);
+(() => {
+  for (let row = 0; row < SIDE; row++) {
+    for (let col = 0; col < SIDE; col++) {
+      const index = getIndex(row, col);
+      toRow[index] = row;
+      toCol[index] = col;
+    }
+  }
+})();
+const cellDrawer = (mask) => {
+  return (cell, offset) => {
+    let unrenderedCell = (cell ^ renderedUnits[offset]) & mask;
+    while (unrenderedCell != 0) {
+      const lz = Math.clz32(unrenderedCell);
+      unrenderedCell ^= 0x80000000 >>> lz;
+      const index = 15 - (lz >>> 1) + offset * 16;
+      const row = toRow[index];
+      const col = toCol[index];
+      const text = index % 9 + 1;
+      ctx.strokeText(
+        text,
+        col * (CELL_SIZE + 1) + CELL_SIZE / 2 + 1,
+        row * (CELL_SIZE + 1) + CELL_SIZE - 1,
+      );
+    }
+  };
+};
+const trueCellDrawer = cellDrawer(0xAAAAAAAA);
+const falseCellDrawer = cellDrawer(0x55555555);
 const drawCells = () => {
   const cells = units();
+  if (!cells.some((v, i) => v !== renderedUnits[i])) {
+    return;
+  }
 
   ctx.font = CELL_SIZE + "px sans-serif";
   ctx.textAlign = "center";
 
   ctx.strokeStyle = TRUE_COLOR;
-  for (let row = 0; row < SIDE; row++) {
-    for (let col = 0; col < SIDE; col++) {
-      const idx = getIndex(row, col);
-      const value = (cells[idx / 16 | 0] >> (idx % 16 * 2)) & 3;
-      if (value !== 2) {
-        continue;
-      }
-      const text = idx % 9 + 1;
-      ctx.strokeText(
-        text,
-        col * (CELL_SIZE + 1) + CELL_SIZE / 2 + 1,
-        row * (CELL_SIZE + 1) + CELL_SIZE - 1,
-      );
-    }
-  }
+  cells.forEach(trueCellDrawer);
 
   ctx.strokeStyle = FALSE_COLOR;
-  for (let row = 0; row < SIDE; row++) {
-    for (let col = 0; col < SIDE; col++) {
-      const idx = getIndex(row, col);
-      const value = (cells[idx / 16 | 0] >> (idx % 16 * 2)) & 3;
-      if (value !== 1) {
-        continue;
-      }
-      const text = idx % 9 + 1;
-      ctx.strokeText(
-        text,
-        col * (CELL_SIZE + 1) + CELL_SIZE / 2 + 1,
-        row * (CELL_SIZE + 1) + CELL_SIZE - 1,
-      );
-    }
-  }
+  cells.forEach(falseCellDrawer);
+
+  renderedUnits.set(cells);
 };
 
 canvas.addEventListener("click", event => {
@@ -110,8 +122,9 @@ solveButton.addEventListener("click", event => {
 const genButton = document.getElementById("generate");
 genButton.addEventListener("click", event => {
   const seed = Math.random() * 0xffffffff | 0;
+  cancelAnimationFrame(renderLoopRequestID);
   generate_instance(seed);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(drawGrid);
 });
 
-requestAnimationFrame(renderLoop);
+requestAnimationFrame(drawGrid);
